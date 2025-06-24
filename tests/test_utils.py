3c77a5ed600e0ed3d3c42d1862ed3111d75968f0
Sarabjot Singh <sarabjot@lightup.ai>
2025-06-24 10:33:51 -0700

    add key

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import os
 
-from lightudq.utils import read_markdown_to_text, read_pdf_to_text
+from lightudq.utils import read_pdf_to_text
 
 DOC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "doc_samples"))
 
@@ -10,9 +10,4 @@ class TestUtils:
         file_path = f"{DOC_DIR}/base_description.pdf"
         text = read_pdf_to_text(file_path)
 
-        assert text
-
-    def test_read_markdown_to_text(self):
-        file_path = f"{DOC_DIR}/snowflake.md"
-        text = read_markdown_to_text(file_path)
         assert text
